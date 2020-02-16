aae6d18f16b1ee5425a9816246d881d83025a98a
CYX-code <1755899681@qq.com>
2020-02-16 17:48:41 +0800

    C++实现线性表的链式存储结构（循环链表）

---
+++ C++/线性表的链式存储结构（循环链表）/CircularLinkList.h
@@ -0,0 +1,401 @@
+/*
+        @作者:        CYX
+        @文件:        CircularLinkList.cpp
+        @时间:        2020/2/14 20:22
+*/
+
+#ifndef MY_DS_CIRCULARLINKLIST_H
+#define MY_DS_CIRCULARLINKLIST_H
+
+
+#include <iostream>
+
+using namespace std;
+
+template<class T>
+class CircularLinkList;
+
+template<class T>
+class LinkNode {
+    friend class CircularLinkList<T>;
+
+private:
+    T data;
+    LinkNode<T> *next;
+
+public:
+    LinkNode() {
+        next = NULL;
+    }
+
+    explicit LinkNode(T p) {
+        data = p;
+        next = NULL;
+    }
+
+    LinkNode<T> &operator=(const LinkNode<T> *p) {
+        data = p->data;
+        next = p->next;
+        return *this;
+    }
+
+    T s()
+    {
+        return data;
+    }
+};
+
+template<class T>
+class CircularLinkList {
+private:
+    LinkNode<T> *head;
+    LinkNode<T> *rear;
+
+public:
+    CircularLinkList();                                      //构造函数
+    CircularLinkList(CircularLinkList<T> &list);             //拷贝构造函数
+    ~CircularLinkList();                                     //析构函数
+    void append_first(T p);                                  //头插法，在循环链表头插入数据
+    void append_last(T p);                                   //尾插法，在循环链表尾插入数据
+    bool append_pos(int pos, T p);                           //在循环链表第pos个位置插入数据
+    void pop_first();                                        //删除循环链表头数据
+    void pop_last();                                         //删除循环链表尾数据
+    bool pop_pos(int pos);                                   //删除循环链表中第pos个数据
+    void change_first(T p);                                  //修改循环链表头数据
+    void change_last(T p);                                   //修改循环链表尾数据
+    bool change_pos(int pos, T p);                           //修改循环链表中第pos个数据
+    bool get_first_node(LinkNode<T> *&p);                    //得到循环链表头结点
+    bool get_last_node(LinkNode<T> *&p);                     //得到循环链表尾结点
+    bool get_pos_node(int pos, LinkNode<T> *&p);             //得到循环链表中第pos个结点
+    bool get_first_data(T &p);                               //得到循环链表头数据
+    bool get_last_data(T &p);                                //得到循环链表尾数据
+    bool get_pos_data(int pos, T &p);                        //得到循环链表中第pos个数据
+    int search(T p);                                         //返回循环链表中p相同的第一个元素的下标pos，若不存在则返回-1
+    bool isEmpty();                                          //判断循环链表是否为空
+    int length();                                            //返回循环链表的长度
+    void print();                                            //打印循环链表
+    void clear();                                            //清空循环链表
+};
+
+//构造函数
+template<class T>
+CircularLinkList<T>::CircularLinkList() {
+    head = new LinkNode<T>();
+    rear = new LinkNode<T>();
+    head->next = head;
+    rear = head;
+}
+
+//拷贝构造函数
+template<class T>
+CircularLinkList<T>::CircularLinkList(CircularLinkList<T> &list) {
+
+}
+
+//析构函数
+template<class T>
+CircularLinkList<T>::~CircularLinkList() {
+    clear();
+}
+
+//头插法，在循环链表头插入数据
+template<class T>
+void CircularLinkList<T>::append_first(T p) {
+    //创建结点
+    LinkNode<T> *temp;
+    temp = new LinkNode<T>(p);
+    //判断链表是否为空
+    if (isEmpty()) {
+        rear = temp;
+        head->next = temp;
+        temp->next = temp;
+    } else {
+        temp->next = head->next;
+        head->next = temp;
+        rear->next = temp;
+    }
+}
+
+//尾插法，在循环链表尾插入数据
+template<class T>
+void CircularLinkList<T>::append_last(T p) {
+    //创建结点
+    LinkNode<T> *temp;
+    temp = new LinkNode<T>(p);
+    //判断链表是否为空
+    if (isEmpty()) {
+        rear = temp;
+        head->next = temp;
+        temp->next = temp;
+    } else {
+        rear->next = temp;
+        temp->next = head->next;
+        rear = temp;
+    }
+}
+
+//判断循环链表是否为空
+template<class T>
+bool CircularLinkList<T>::isEmpty() {
+    return head->next == head;
+}
+
+//返回循环链表的长度
+template<class T>
+int CircularLinkList<T>::length() {
+    int len = 0;
+    if (isEmpty())           //空链表时返回0
+        return 0;
+
+    LinkNode<T> *p = head;
+    while (p != rear) {
+        len++;
+        p = p->next;
+    }
+    return len;
+}
+
+//打印循环链表
+template<class T>
+void CircularLinkList<T>::print() {
+    if (isEmpty())
+        return;
+    LinkNode<T> *p = head->next;
+    while (p != rear) {
+        cout << p->data << " ---> ";
+        p = p->next;
+    }
+    cout << p->data << endl;
+}
+
+//清空循环链表
+template<class T>
+void CircularLinkList<T>::clear() {
+    if(isEmpty())
+        return;
+
+    LinkNode<T> *temp;
+
+    while (head->next != rear) {
+        temp = head->next;
+        head->next = temp->next;
+        delete temp;
+    }
+    delete head->next;
+    head->next = head;
+    rear = head;
+}
+
+//在循环链表第pos个位置插入数据
+template<class T>
+bool CircularLinkList<T>::append_pos(int pos, T p) {
+    if (pos <= 0 || pos > length())   //不存在指定位置
+        return false;
+
+    if (pos == 1)
+        append_first(p);
+    else {
+        //创建结点
+        LinkNode<T> *temp;
+        temp = new LinkNode<T>(p);
+
+        LinkNode<T> *q = head;
+        //得到指定位置的前驱节点
+        for (int i = 0; i < pos - 1; i++)
+            q = q->next;
+
+        temp->next = q->next;
+        q->next = temp;
+    }
+    return true;
+}
+
+//删除循环链表头数据
+template<class T>
+void CircularLinkList<T>::pop_first() {
+    if (isEmpty())     //空链表
+        return;
+    //只有一个结点时
+    if (length() == 1) {
+        delete head->next;
+        head->next = head;
+        rear = head;
+    } else {
+        LinkNode<T> *temp;
+        temp = head->next;
+
+        rear->next = temp->next;
+        head->next = temp->next;
+        delete temp;
+    }
+}
+
+//删除循环链表尾数据
+template<class T>
+void CircularLinkList<T>::pop_last() {
+    if (isEmpty())     //空链表
+        return;
+    //只有一个结点时
+    if (length() == 1) {
+        delete head->next;
+        head->next = head;
+        rear = head;
+    } else {
+        LinkNode<T> *temp;
+
+        LinkNode<T> *q = head->next;
+        while(q->next != rear)        //循环得到尾结点的前驱节点
+            q = q->next;
+
+        rear = q;
+        temp = q->next;
+        q->next = head->next;
+        delete temp;
+    }
+}
+
+//删除循环链表中第pos个数据
+template<class T>
+bool CircularLinkList<T>::pop_pos(int pos) {
+    if (pos <= 0 || pos > length())   //不存在指定位置
+        return false;
+    //pos == 1即删除首结点
+    if(pos == 1)
+        pop_first();
+    //pos == length()即删除尾结点
+    else if(pos == length())
+        pop_last();
+    else
+    {
+        LinkNode<T> *temp;
+        //循环得到待删除结点的前驱节点
+        LinkNode<T> *q = head;
+        for(int i=0;i<pos-1;i++)
+            q = q->next;
+
+        temp = q->next;
+        q->next = temp->next;
+        delete temp;
+    }
+    return true;
+}
+
+//修改循环链表头数据
+template<class T>
+void CircularLinkList<T>::change_first(T p) {
+    if(isEmpty())    //空链表
+        return;
+    head->next->data = p;
+}
+
+//修改循环链表尾数据
+template<class T>
+void CircularLinkList<T>::change_last(T p) {
+    if(isEmpty())    //空链表
+        return;
+    rear->data = p;
+}
+
+//修改循环链表中第pos个数据
+template<class T>
+bool CircularLinkList<T>::change_pos(int pos, T p) {
+    if (pos <= 0 || pos > length())   //不存在指定位置
+        return false;
+
+    //循环得到待修改的结点
+    LinkNode<T> *q = head;
+    for(int i=0;i<pos;i++)
+        q = q->next;
+
+    q->data = p;
+
+    return true;
+}
+
+//得到循环链表头结点
+template<class T>
+bool CircularLinkList<T>::get_first_node(LinkNode<T> *&p) {
+    if(isEmpty())    //空链表
+        return false;
+    p = head->next;
+    return true;
+}
+
+//得到循环链表尾结点
+template<class T>
+bool CircularLinkList<T>::get_last_node(LinkNode<T> *&p) {
+    if(isEmpty())    //空链表
+        return false;
+    p = rear;
+    return true;
+}
+
+//得到循环链表中第pos个结点
+template<class T>
+bool CircularLinkList<T>::get_pos_node(int pos, LinkNode<T> *&p) {
+    if (pos <= 0 || pos > length())   //不存在指定位置
+        return false;
+
+    //循环得到第pos个结点
+    LinkNode<T> *q = head;
+    for(int i=0;i<pos;i++)
+        q = q->next;
+    p = q;
+
+    return true;
+}
+
+//得到循环链表头数据
+template<class T>
+bool CircularLinkList<T>::get_first_data(T &p) {
+    LinkNode<T> *temp;
+    if(get_first_node(temp))
+    {
+        p = temp->data;
+        return true;
+    }
+    return false;
+}
+
+//得到循环链表尾数据
+template<class T>
+bool CircularLinkList<T>::get_last_data(T &p) {
+    LinkNode<T> *temp;
+    if(get_last_node(temp))
+    {
+        p = temp->data;
+        return true;
+    }
+    return false;
+}
+
+//得到循环链表中第pos个数据
+template<class T>
+bool CircularLinkList<T>::get_pos_data(int pos, T &p) {
+    LinkNode<T> *temp;
+    if(get_pos_node(pos,temp))
+    {
+        p = temp->data;
+        return true;
+    }
+    return false;
+}
+
+//返回循环链表中p相同的第一个元素的下标pos，若不存在则返回-1
+template<class T>
+int CircularLinkList<T>::search(T p) {
+    if(isEmpty())      //空链表
+        return -1;
+    int pos = 0;       //记录下标
+    LinkNode<T> *q = head->next;
+    while(q != rear)
+    {
+        if(q->data == p)
+            return pos;
+        q = q->next;
+        pos++;
+    }
+    return -1;
+}
+
+#endif //MY_DS_CIRCULARLINKLIST_H
---
+++ C++/线性表的链式存储结构（循环链表）/CircularLinkListMain.cpp
@@ -0,0 +1,147 @@
+/*
+        @作者:        CYX
+        @文件:        CircularLinkListMain.cpp
+        @时间:        2020/2/14 20:24
+*/
+
+#include <iostream>
+#include "CircularLinkList.h"
+
+using namespace std;
+
+CircularLinkList<int> myList;
+
+void menu() {
+    cout << "\t*****************************************************" << endl;
+    cout << "\t*     1.头插法插入数据        2.尾插发插入数据         *" << endl;
+    cout << "\t*     3.插入到第pos个位置                             *" << endl;
+    cout << "\t*     4.删除表头数据          5.删除表尾数据           *" << endl;
+    cout << "\t*     6.删除表中第pos个数据                           *" << endl;
+    cout << "\t*     7.修改表头数据          8.修改表尾数据           *" << endl;
+    cout << "\t*     9.修改表中第pos个数据                           *" << endl;
+    cout << "\t*    10.输出表头数据         11.输出表尾数据           *" << endl;
+    cout << "\t*    12.输出表中第pos个数据                           *" << endl;
+    cout << "\t*    13.查找数据             14.得到表长度            *" << endl;
+    cout << "\t*    15.打印表中数据                                  *" << endl;
+    cout << "\t*     0.退出程序                                     *" << endl;
+    cout << "\t*****************************************************" << endl;
+}
+
+//输入函数
+template<class T>
+void input(const string &str, T &x) {
+    cout << "请输入" << str;
+    //cout<<"Please input"<<str<<"data: ";
+    cin >> x;
+}
+
+void Realization(int sel) {
+    int data;      //待插入、删除、输出或修改后的数据
+    int x;         //插入、修改前的数据
+    int pos;       //待插入、删除或修改的位置
+    switch (sel) {
+        case 0:
+            cout << "\t程序退出" << endl;
+            break;
+        case 1:
+            cout << "\t头插法插入数据" << endl;
+            input("待插入的数据：", data);
+            myList.append_first(data);
+            break;
+        case 2:
+            cout << "\t尾插发插入数据" << endl;
+            input("待插入的数据：", data);
+            myList.append_last(data);
+            break;
+        case 3:
+            cout << "\t插入到第pos个位置" << endl;
+            input("位置pos：", x);
+            input("待插入的数据：", data);
+            if (!myList.append_pos(x, data))
+                cout << "插入失败" << endl;
+            break;
+        case 4:
+            cout << "\t删除表头数据" << endl;
+            myList.pop_first();
+            break;
+        case 5:
+            cout << "\t删除表尾数据" << endl;
+            myList.pop_last();
+            break;
+        case 6:
+            cout << "\t删除表中第pos个数据" << endl;
+            input("待删除数据位置pos：", data);
+            if (!myList.pop_pos(data))
+                cout << "删除失败" << endl;
+            break;
+        case 7:
+            cout << "\t修改表头数据" << endl;
+            input("修改后的数据：", data);
+            myList.change_first(data);
+            break;
+        case 8:
+            cout << "\t修改表尾数据" << endl;
+            input("修改后的数据：", data);
+            myList.change_last(data);
+            break;
+        case 9:
+            cout << "\t修改表中第pos个数据" << endl;
+            input("位置pos：", pos);
+            input("修改后的数据：", data);
+            if (!myList.change_pos(pos, data))
+                cout << "修改失败" << endl;
+            break;
+        case 10:
+            cout << "\t输出表头数据" << endl;
+            if (myList.get_first_data(data))
+                cout << "表头数据：" << data << endl;
+            break;
+        case 11:
+            cout << "\t输出表尾数据" << endl;
+            if (myList.get_last_data(data))
+                cout << "表头数据：" << data << endl;
+            break;
+        case 12:
+            cout << "\t输出表中第pos个数据" << endl;
+            input("待输出数据位置：", pos);
+            if (myList.get_pos_data(pos, data))
+                cout << "表中第pos个数据：" << data << endl;
+            break;
+        case 13: {
+            cout << "\t查找数据" << endl;
+            input("待查找数据：", data);
+            int is = myList.search(data);
+            if (is == -1)
+                cout << "查找失败" << endl;
+            else
+                cout << "查找数据的下标为：" << is << endl;
+            break;
+        }
+        case 14:
+            cout << "\t得到表长度" << endl;
+            cout << "表长：" << myList.length() << endl;
+            break;
+        case 15:
+            cout << "\t打印表中数据" << endl;
+            myList.print();
+            cout << endl;
+            break;
+        default:
+            break;
+    }
+}
+
+int main() {
+    int sel;
+    while (true) {
+        menu();
+        cout << "选择功能：";
+        cin >> sel;
+        Realization(sel);
+        cout<<endl;
+        if (sel == 0)
+            break;
+    }
+
+    return 0;
+}
